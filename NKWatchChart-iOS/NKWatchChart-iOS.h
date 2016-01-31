a2936bad74f79f635090a67eeb2d72bd0d46752d
Igor Palaguta <igor.palaguta@gmail.com>
2016-01-31 23:16:28 +0200

    Add frameworkds

---
+++ NKWatchChart-iOS/NKWatchChart-iOS.h
@@ -0,0 +1,19 @@
+//
+//  NKWatchChart-iOS.h
+//  NKWatchChart-iOS
+//
+//  Created by Igor Palaguta on 31.01.16.
+//  Copyright © 2016 Peng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NKWatchChart-iOS.
+FOUNDATION_EXPORT double NKWatchChart_iOSVersionNumber;
+
+//! Project version string for NKWatchChart-iOS.
+FOUNDATION_EXPORT const unsigned char NKWatchChart_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NKWatchChart_iOS/PublicHeader.h>
+
+
---
+++ NKWatchChart-watchOS/NKWatchChart-watchOS.h
@@ -0,0 +1,19 @@
+//
+//  NKWatchChart-watchOS.h
+//  NKWatchChart-watchOS
+//
+//  Created by Igor Palaguta on 31.01.16.
+//  Copyright © 2016 Peng. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for NKWatchChart-watchOS.
+FOUNDATION_EXPORT double NKWatchChart_watchOSVersionNumber;
+
+//! Project version string for NKWatchChart-watchOS.
+FOUNDATION_EXPORT const unsigned char NKWatchChart_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NKWatchChart_watchOS/PublicHeader.h>
+
+
